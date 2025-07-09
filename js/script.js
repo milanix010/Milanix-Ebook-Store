f1ee753261d734efd4603879d5b97289c38fd13b
milanix010 <milanix010@gmail.com>
2025-07-09 23:27:03 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 $(document).ready(function () {
+  // Search books by title
   $('#searchBox').on('keyup', function () {
-    let query = $(this).val().toLowerCase();
+    const query = $(this).val().toLowerCase();
     $('.book').each(function () {
       const title = $(this).data('title').toLowerCase();
       $(this).toggle(title.includes(query));
     });
   });
 
+  // Filter books by genre
   $('#genreFilter').on('change', function () {
-    let genre = $(this).val();
+    const genre = $(this).val();
     $('.book').each(function () {
       if (genre === "" || $(this).data('genre') === genre) {
         $(this).show();
